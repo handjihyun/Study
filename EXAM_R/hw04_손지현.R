1fb26c3dc4594963830fb4cf962a4df49de44f0b
handjihyun <handjihyun00@gmail.com>
2023-02-13 18:03:38 +0900

    과제 수정

--- EXAM_R/hw04_손지현.R
+++ EXAM_R/hw04_손지현.R
@@ -19,8 +19,7 @@ polygon(x/4-0.7, y/2+0.7, col = '#fea9d7')
 angle <- 200
 rotation_matrix <- matrix(c(cos(angle), -sin(angle), sin(angle), cos(angle)), ncol = 2)
 xy_rotated <- as.matrix(cbind(x/2+0.75, y/4+0.6)) %*% rotation_matrix
-polygon(xy_rotated[,1], xy_rotated[,2], col = '#fea9d7', lty = 0)
-#polygon(x/2+0.75, y/4+0.2, col = '#fea9d7')
+polygon(xy_rotated[,1], xy_rotated[,2], col = '#fea9d7')
 
 polygon(x, y, col = '#fea9d7')
 polygon(x/8+0.2, y/3+0.5, col = '#0b0a13', lty = 0)
@@ -36,4 +35,3 @@ polygon(x/6-0.5, y/8+0.2, col = '#f95fa8', lty = 0)
 polygon(x/6+0.5, y/8+0.2, col = '#f95fa8',lty = 0)
 
 polygon(c(-0.2,0.2,0), c(0.1,0.1,-0.1), col = '#f3212d',lty = 0)
-
