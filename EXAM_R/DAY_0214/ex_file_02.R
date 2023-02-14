da433bcff29f8344372da7f4115526db54d8705f
handjihyun <handjihyun00@gmail.com>
2023-02-14 19:27:50 +0900

    r 과제

--- EXAM_R/DAY_0214/ex_file_02.R
+++ EXAM_R/DAY_0214/ex_file_02.R
@@ -11,9 +11,23 @@ str(popData)
 table(popData$gender)
 table(popData$relationship)
 
+# ==================================================
+# Data Summary - Graph
+# ==================================================
+# --------------------------------------------------
 # categorical data frequency expression >> bar graph
+# --------------------------------------------------
 barplot(table(popData$gender))
 barplot(table(popData$relationship))
 
+# --------------------------------------------------
 # draw bar graph the number of boy babies born
+# --------------------------------------------------
 barplot(table(popData$boy))
+
+# --------------------------------------------------
+# draw histogram
+# - hist(data, prbability=T, breaks=c(), right=F,
+#        main= , xlab= , ylab= )
+# --------------------------------------------------
+hist(table(popData$age), breaks = c(seq(0:80, by = 10)))
